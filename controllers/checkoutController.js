932e6319ae85ee3da93f33bc1fb6c8781982fb28
Andile Semela <issbonqueque@gmail.com>
2026-06-11 22:57:19 +0200

    Controllers implementation

---
+++ controllers/checkoutController.js
@@ -0,0 +1,29 @@
+// review order
+exports.reviewOrder = (req, res) => {
+    res.json({ message: "Review order"});
+};
+
+// process checkout
+exports.processCheckout = (req, res) => {
+    res.json({ message: "Checkout Process"});
+};
+
+// process payment
+exports.processpayment = (req, res) => {
+    res.json({ message: "Payment process"});
+};
+
+// process payment
+exports.processpayment = (req, res) => {
+    res.json({ message: "Payment process"});
+};
+
+// order confirmation
+exports.orderConfirmation = (req, res) => {
+    res.json({ message: "Order confirmed"});
+};
+
+// cancel checkout
+exports.cancelCheckout = (req, res) => {
+    res.json({ message: "Checkout cancelled"});
+};
---
+++ controllers/statusController.js
@@ -0,0 +1,24 @@
+// order status
+exports.getOrderStatus = (req, res) => {
+    res.json({ message: "Order status retrieved"});
+};
+
+// marked for preparing
+exports.markPreparing = (req, res) => {
+    res.json({ message: "Order status retrieved"});
+};
+
+// marked for preparing
+exports.markPreparing = (req, res) => {
+    res.json({ message: "Order status retrieved"});
+};
+
+// mark delivery
+exports.markDelivered = (req, res) => {
+    res.json({ message: "Order marked as delivered"});
+};
+
+// mark cancelled
+exports.markCencelled = (req, res) => {
+    res.json({ message: "Order marked as cancelled"});
+};
